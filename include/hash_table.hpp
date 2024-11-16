cbc4fd6e7db3b529d91841a1710347b3cea519a7
Ruslan Sedov <54082165+SakuraWhisper@users.noreply.github.com>
2024-11-16 19:57:52 +0300

    fix

--- include/hash_table.hpp
+++ include/hash_table.hpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#pragma once
+
+
+template <typename Value>
+struct Node {
+    size_t key;
+    Value value;
+    Node* next;
+
+    Node(const size_t k, const Value& v) : key(k), value(v), next(nullptr) {}
+};
 
 template <typename Value>
 class HashTable {
 private:
-    struct Node {
-        size_t key;
-        Value value;
-        Node* next;
-
-        Node(const size_t k, const Value& v) : key(k), value(v), next(nullptr) {}
-    };
-
-    Node** table; // ”казатель на массив указателей на узлы
+    Node<Value>** table; // ”казатель на массив указателей на узлы
     size_t size; // size table
     size_t count; // count elements
 
@@ -21,8 +24,35 @@ private:
     }
 
 public:
+    //using iterator = HashTableIterator<Value>;
+    //using const_iterator = HashTableIterator<const Value>;
+    //using reverse_iterator = std::reverse_iterator<Iterator<Value>>;
+    //using const_reverse_iterator = std::reverse_iterator<HashTableIterator<const Value>>;
+
+    //iterator begin() {
+    //    size_t first_el = 0;
+    //    
+    //    while (find(first_el) == nullptr) {
+    //        ++first_el;
+    //    }
+    //    return iterator(head); 
+    //}
+    /*iterator end() { return iterator(tail->next); }
+    iterator begin() const { return iterator(head); }
+    iterator end() const { return iterator(tail->next); }
+    const_iterator cbegin() const { return head; }
+    const_iterator cend() const { return tail->next; }
+    reverse_iterator rbegin() { return std::make_reverse_iterator(tail); }
+    reverse_iterator rend() { return std::make_reverse_iterator(head); }
+    reverse_iterator rbegin() const { return std::make_reverse_iterator(rbegin()); }
+    reverse_iterator rend() const  { return std::make_reverse_iterator(rend()); }
+    const_reverse_iterator crbegin() const { return rend(); }
+    const_reverse_iterator crend() const { return rbegin(); }*/
+
+
+
     HashTable(size_t capacity) : size(capacity), count(0) {
-        table = new Node * [size];
+        table = new Node<Value>* [size];
         for (size_t i = 0; i < size; ++i) {
             table[i] = nullptr;
         }
@@ -30,9 +60,9 @@ public:
 
     ~HashTable() {
         for (size_t i = 0; i < size; ++i) {
-            Node* current = table[i];
+            Node<Value>* current = table[i];
             while (current != nullptr) {
-                Node* next = current->next;
+                Node<Value>* next = current->next;
                 delete current;
                 current = next;
             }
@@ -45,18 +75,18 @@ public:
             return;
         }
         size_t index = hash(key);
-        Node* newNode = new Node(key, value);
+        Node<Value>* newNode = new Node(key, value);
         newNode->next = table[index];
         table[index] = newNode;
         count++;
     }
 
-    Value* find(const size_t key) const {
+    Node<Value>* find(const size_t key) const {
         size_t index = hash(key);
-        Node* current = table[index];
+        Node<Value>* current = table[index];
         while (current != nullptr) {
             if (current->key == key) {
-                return &(current->value);
+                return current;
             }
             current = current->next;
         }
@@ -65,8 +95,8 @@ public:
 
     void erase(const size_t key) {
         size_t index = hash(key);
-        Node* current = table[index];
-        Node* previous = nullptr;
+        Node<Value>* current = table[index];
+        Node<Value>* previous = nullptr;
         while (current != nullptr) {
             if (current->key == key) {
                 if (previous != nullptr) {
@@ -87,14 +117,14 @@ public:
     HashTable<Value> unionWith(const HashTable<Value>& other) const {
         HashTable<Value> result(size > other.size ? size : other.size);
         for (size_t i = 0; i < size; ++i) {
-            Node* current = table[i];
+            Node<Value>* current = table[i];
             while (current) {
                 result.insert(current->key, current->value);
                 current = current->next;
             }
         }
         for (size_t i = 0; i < other.size; ++i) {
-            Node* current = other.table[i];
+            Node<Value>* current = other.table[i];
             while (current) {
                 result.insert(current->key, current->value);
                 current = current->next;
@@ -103,11 +133,10 @@ public:
         return result;
     }
 
-    //ok
     HashTable<Value> intersectionWith(const HashTable<Value>& other) const {
         HashTable<Value> result(size < other.size ? size : other.size);
         for (size_t i = 0; i < result.size; ++i) {
-            Node* current = table[i];
+            Node<Value>* current = table[i];
             while (current) {
                 //if the element is not in the second set, it does not fit
                 if (other.find(current->key) == nullptr) {
@@ -124,7 +153,7 @@ public:
     HashTable<Value> differenceWith(const HashTable<Value>& other) const {
         HashTable<Value> result(size);
         for (size_t i = 0; i < size; ++i) {
-            Node* current = table[i];
+            Node<Value>* current = table[i];
             while (current) {
                 //if the element is not in the second set, it is suitable
                 if (other.find(current->key) == nullptr) {
@@ -139,7 +168,7 @@ public:
     HashTable<Value> symetricDifferenceWith(const HashTable<Value>& other) const {
         HashTable<Value> result(size);
         for (size_t i = 0; i < size; ++i) {
-            Node* current = table[i];
+            Node<Value>* current = table[i];
             while (current) {
                 //insert what is in the first set but not in the second
                 if (other.find(current->key) == nullptr) {
@@ -150,7 +179,7 @@ public:
         }
 
         for (size_t i = 0; i < size; ++i) {
-            Node* current = other.table[i];
+            Node<Value>* current = other.table[i];
             while (current) {
                 //insert what is in the second set but not in the first
                 if (find(current->key) == nullptr) {
@@ -163,7 +192,11 @@ public:
     }
 
     Value* operator[](const size_t key) {
-        return find(key);
+        Node<Value>* el = find(key);
+        if (el == nullptr) {
+            return nullptr;
+        }
+        return &(el->value);
     }
 
     size_t capacity() const {
@@ -176,7 +209,7 @@ public:
             return false;
         }
         for (size_t i = 0; i < size; ++i) {
-            Node* current = table[i];
+            Node<Value>* current = table[i];
             while (current) {
                 //if you find an element that is not in the second set
                 if (other.find(current->key) == nullptr) {
@@ -191,7 +224,7 @@ public:
     void addingFirstMissingOne() {
         size_t firstMissingOneKey = 0;
         for (size_t i = 0; i < size; ++i) {
-            Node* current = table[i];
+            Node<Value>* current = table[i];
             while (current) {
                 if (find(firstMissingOneKey) == nullptr) {
                     // (0, 0) (3, 3) -> (0, 0) (1, 1) (3, 3)
@@ -210,7 +243,7 @@ public:
     void print() const {
         for (size_t i = 0; i < size; ++i) {
             std::cout << "Index " << i << ": ";
-            Node* current = table[i];
+            Node<Value>* current = table[i];
             while (current != nullptr) {
                 std::cout << "(" << current->key << ", " << current->value << ") ";
                 current = current->next;
@@ -223,7 +256,7 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const HashTable<Value>& ht) {
         os << "Set: ";
         for (size_t i = 0; i < ht.size; ++i) {
-            Node* current = ht.table[i];
+            Node<Value>* current = ht.table[i];
             while (current != nullptr) {
                 os << current->value << " ";
                 current = current->next;
@@ -246,3 +279,47 @@ public:
         return is;
     }
 };
+
+//template<typename Value>
+//class HashTableIterator {
+//public:
+//    using difference_type = std::ptrdiff_t;
+//    using value_type = std::remove_cv_t<Value>;
+//    using pointer = Type*;
+//    using reference = Type&;
+//    using iterator_category = std::random_access_iterator_tag;
+//    using iterator_concept = std::contiguous_iterator_tag;
+//
+//    explicit HashTableIterator(Node<Value>* ptr = nullptr) : date(ptr) {}
+//
+//    HashTableIterator(const HashTableIterator& iterator) : HashTableIterator(iterator.date) {}
+//
+//    ~HashTableIterator() = default;
+//
+//    bool operator==(const HashTableIterator& lhs) const {
+//        return date == lhs.date;
+//    }
+//
+//    bool operator!=(const HashTableIterator& lhs) const {
+//        return date != lhs.date;
+//    }
+//
+//    HashTableIterator operator++() {
+//       
+//    }
+//
+//    HashTableIterator operator++(int post) {
+//
+//    }
+//    HashTableIterator operator--() {
+//
+//    }
+//    HashTableIterator operator--(int post) {
+//
+//    }
+//    Value& operator*() {
+//
+//    }
+//private:
+//    Node<Value>* date;
+//};
